3ef936fc2e30051edd38e5a6eac6caa56a84f768
ShenDezhou <bangtech@sina.com>
2018-09-09 17:09:12 +0800

    fix EOF variable substitution.

--- deploy_shadowsocks.sh
+++ deploy_shadowsocks.sh
@@ -1,10 +1,10 @@
 yum install -y git
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
-cat << EOF > ss_god.sh
+cat << 'EOF' > ss_god.sh
 for ip in {1..40}
 do
-        ssserver -p \$ip -k hk.wwwsto -m aes-256-cfb &
+        ssserver -p $ip -k hk.wwwsto -m aes-256-cfb &
         sleep 1
 done
-EOF
+'EOF'
 nohup sh ss_god.sh 2>&1 1>/dev/null &
