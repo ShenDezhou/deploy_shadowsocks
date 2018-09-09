260797ffef449ee796c64fe045338f2ef610b1db
ShenDezhou <bangtech@sina.com>
2018-09-09 17:43:29 +0800

    use $() to uninterprete.

--- deploy_shadowsocks.sh
+++ deploy_shadowsocks.sh
@@ -1,9 +1,9 @@
 yum install -y git
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
- sh -c "cat > ss_god.sh" <<EOF
+cat <<EOF > ss_god.sh
 for ip in {1..40}
 do
-        ssserver -p $ip -k hk.wwwsto -m aes-256-cfb &
+        ssserver -p $(ip) -k hk.wwwsto -m aes-256-cfb &
         sleep 1
 done
 EOF
